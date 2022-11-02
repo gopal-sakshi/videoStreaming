019a628333a392a38e442370e254d3adada5938d
gopal-sakshi <gopal612@gmail.com>
2022-11-02 15:01:28 +0530

    pushed video

--- index.js
+++ index.js
@@ -28,27 +28,46 @@ app.get('/videos/:videoId', (req, res) => {
     const range = req.headers.range;
     if (!range) {
         console.log('set the range');
-        // res.status(400).send("Requires Range header");
+        res.status(400).send("Requires Range header");
     }
 
+    var videoDetails = fetchVideo(videoId, range);
+
+    const headers = {
+        "Content-Range": `bytes ${videoDetails.start}-${videoDetails.end}/${videoDetails.videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": videoDetails.contentLength,
+        "Content-Type": `video/${videoDetails.videoExtension}`,
+    };
+    res.writeHead(206, headers);
+    const videoStart = videoDetails.start;
+    const videoEnd = videoDetails.end;
+    const videoStream = fs.createReadStream(__dirname+`${videoDetails.videoPath}`, { videoStart, videoEnd });
+    videoStream.pipe(res);
+});
+
+function fetchVideo(videoId, range) {
     // videoPath & videoSize
-    const videoPath = `/resources/${videoId}.webm`;
+    var videoExtension = getVideoExtension();
+    const videoPath = `/resources/${videoId}.${videoExtension}`;
     const videoSize = fs.statSync(__dirname+`${videoPath}`).size;
     console.log(videoPath);
     console.log(videoSize);
 
-    const CHUNK_SIZE = 10 ** 6; // 1MB              // it seems ** -------> is powerOf ????
+    const CHUNK_SIZE = 100 ** 6; // 1MB              // it seems ** -------> is powerOf ????
     const start = Number(range.replace(/\D/g, ""));
     const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
     const contentLength = end - start + 1;
+    return {
+        videoPath: videoPath,
+        videoSize: videoSize,
+        start: start,
+        end: end,
+        contentLength: contentLength,
+        videoExtension: videoExtension
+    }
+}
 
-    const headers = {
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": contentLength,
-        "Content-Type": "video/webm",        
-    };
-    res.writeHead(206, headers);
-    const videoStream = fs.createReadStream(__dirname+`${videoPath}`, { start, end });
-    videoStream.pipe(res);
-});
+function getVideoExtension() {
+    return 'mp4';
+}
